3a039bbc4a3239e952f411cb3be3b5d040c7415b
Valdymarych <107954331+Valdymarych@users.noreply.github.com>
2022-06-23 15:07:23 +0300

    ще покращив SimpleNoise

--- PerlinNoisePackage.py
+++ PerlinNoisePackage.py
@@ -4,104 +4,130 @@ from pygame import *
 import math as mt
 import time as tm
 class SimpleNoise:
-    def __init__(self,WIDTH,HEIGHT,side,smoothing=1):
+    def __init__(self,WIDTH,HEIGHT,side,smoothing=1,zoom=[1,1]):
         self.WIDTH=WIDTH
         self.HEIGHT=HEIGHT
+
         self.side=side
-        self.noiseArray,self.vectors=np.array([]),[]
-        self.noiseSurface=Surface((side,side))
+        self.sideX=side*zoom[0]
+        self.sideY=side*zoom[1]
+
+        self.vectorsWIDTH=self.WIDTH + self.sideX - 1 - (self.WIDTH - 1) % self.sideX
+        self.vectorsHEIGHT=self.HEIGHT + self.sideY - 1 - (self.HEIGHT - 1) % self.sideY
+
+        self.noiseArrayColors=np.array([])  # для малювидла                 (0 - 255)
+        self.noiseArrayValues=np.array([])  # для більш складніших структур (self.minValue - self.maxValue)
+        self.vectors=[]
         self.smoothing=smoothing
 
         self.smooth=lambda t,i: self.smooth(t*t*(3-2*t),i-1) if i>0 else t
+
+        self.minValue=-(2**(-1/2))
+        self.maxValue=2**(-1/2)
         self.generateNoise()
 
-    def generateNoise(self):
-        width = self.WIDTH + self.side - 1 - (self.WIDTH - 1) % self.side
-        height = self.HEIGHT + self.side - 1 - (self.HEIGHT - 1) % self.side
-        vectors = []
+        self.surface=Surface((self.WIDTH,self.HEIGHT))
+
+        self.filters=[]
+
+
+    def generateVectors(self):
         vectorByAngle = lambda alfa: Vector2(mt.sin(alfa), mt.cos(alfa))
-        for y in range(int(height // self.side + 1)):
-            vectors.append([])
-            for x in range(int(width // self.side + 1)):
-                vectors[-1].append(vectorByAngle(rd.random() * mt.pi * 2))
-        result = np.zeros((height, width), dtype=np.float32)
-        sector = np.linspace(0, self.side - 1, self.side, dtype=np.float32) / self.side
-        sectorX, sectorY = np.meshgrid(sector, sector)
+        for y in range(int(self.vectorsHEIGHT // self.sideY + 1)):
+            self.vectors.append([])
+            for x in range(int(self.vectorsWIDTH // self.sideX + 1)):
+                self.vectors[-1].append(vectorByAngle(rd.random() * mt.pi * 2))
+
+    def generateNoise(self):
+        result = np.zeros((self.vectorsHEIGHT, self.vectorsWIDTH), dtype=np.float32)
+        sectorXproto = np.linspace(0, (self.sideX - 1), self.sideX, dtype=np.float32) / self.sideX
+        sectorYproto = np.linspace(0, (self.sideY - 1), self.sideY, dtype=np.float32) / self.sideY
+        sectorX, sectorY = np.meshgrid(sectorXproto, sectorYproto)
         sectorX2 = sectorX - 1
-        sectorZ1 = np.zeros((self.side, self.side), dtype=np.float32)
-        sectorZ2 = np.zeros((self.side, self.side), dtype=np.float32)
-        for y, yVectors in enumerate(vectors[:-1]):
+        sectorZ1 = np.zeros((self.sideY, self.sideX), dtype=np.float32)
+        sectorZ2 = np.zeros((self.sideY, self.sideX), dtype=np.float32)
+        for y, yVectors in enumerate(self.vectors[:-1]):
             for x, xVector in enumerate(yVectors[:-1]):
                 v1x, v1y = xVector.xy
-                v2x, v2y = vectors[y + 1][x].xy
-                v3x, v3y = vectors[y][x + 1].xy
-                v4x, v4y = vectors[y + 1][x + 1].xy
-                sectorZ1[:, :] = v1x * sectorX + v1y * sectorY + self.smooth(sectorY,self.smoothing) * (
-                            sectorX * (v2x - v1x) + sectorY * (v2y - v1y) - v2y)
-                sectorZ2[:, :] = v3x * sectorX2 + v3y * sectorY + self.smooth(sectorY,self.smoothing) * (
-                            sectorX2 * (v4x - v3x) + sectorY * (v4y - v3y) - v4y)
+                v2x, v2y = self.vectors[y + 1][x].xy
+                v3x, v3y = self.vectors[y][x + 1].xy
+                v4x, v4y = self.vectors[y + 1][x + 1].xy
+                sectorZ1[:, :] = v1x * sectorX + v1y * sectorY + self.smooth(sectorY,self.smoothing) * (sectorX * (v2x - v1x) + sectorY * (v2y - v1y) - v2y)
+                sectorZ2[:, :] = v3x * sectorX2 + v3y * sectorY + self.smooth(sectorY,self.smoothing) * (sectorX2 * (v4x - v3x) + sectorY * (v4y - v3y) - v4y)
+                result[y * self.sideY:(y + 1) * self.sideY, x * self.sideX:(x + 1) * self.sideX] = sectorZ1 + (sectorZ2 - sectorZ1) * self.smooth(sectorX,self.smoothing)
+        result = result[:self.HEIGHT, :self.WIDTH]
+        self.noiseArrayValues = result
 
-                result[y * self.side:(y + 1) * self.side, x * self.side:(x + 1) * self.side] = sectorZ1 + (sectorZ2 - sectorZ1) * self.smooth(
-                    sectorX,self.smoothing)
-        result = (result[:self.HEIGHT, :self.WIDTH] - result.min()) / (-result.min() + result.max()) * 255
-        result = result.astype(dtype=np.uint8)
-        result = np.rot90(result)
-        result=np.expand_dims(result,2)
-        self.noiseArray=np.concatenate([result, result, result], 2)
-        self.vectors=vectors
-        self.surface = surfarray.make_surface(self.noiseArray)
+
+    def generateSurface(self):  # робить noiseArrayColors з noiseArrayValues і запихає все в surface
+        self.noiseArrayColors = (self.noiseArrayValues - self.minValue) / (-self.minValue + self.maxValue) * 255
+        self.noiseArrayColors = np.rot90(self.noiseArrayColors)
+        self.noiseArrayColors = np.expand_dims(self.noiseArrayColors,2)
+        self.noiseArrayColors = np.concatenate([self.noiseArrayColors, self.noiseArrayColors, self.noiseArrayColors], 2)
+        self.noiseArrayColors = self.applyFilters(self.noiseArrayColors)
+        self.noiseArrayColors = self.noiseArrayColors.astype(dtype=np.uint8)
+        self.surface = surfarray.make_surface(self.noiseArrayColors)
 
     def getSurface(self):
         return self.surface
 
-class View:
-    def __init__(self,x,y,width,height):
-        self.posts = []
-        self.height=height
-        self.width=width
-        self.y=y
-        self.x=x
-        self.surface=Surface(width,height)
+    def resize(self,newWIDTH,newHEIGHT):
+        self.WIDTH=newWIDTH
+        self.HEIGHT=newHEIGHT
 
-    def update(self,surface):
-        self.updateSelf()
-        self.updatePost()
+    def applyFilters(self,noise):
+        return noise
 
 
-        self.draw(surface)
+class MixNoise:
+    def __init__(self,WIDTH,HEIGHT):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+
+        self.noises=[]
+        self.impacts=[]   # 1  1/2  1/4  1/8 .... (~side)
+        self.noisesCount=0
+
+        self.surface=Surface((self.WIDTH,self.HEIGHT))
+
+        self.minValue=0
+        self.maxValue=0
+
+        self.noiseArrayColors=np.zeros((self.HEIGHT,self.WIDTH,3))  # для малювидла                 (0 - 255)
+        self.noiseArrayValues=np.zeros((self.HEIGHT,self.WIDTH))  # для більш складніших структур (self.minValue - self.maxValue)
 
 
-    def draw(self,surface):
-        self.drawSelf()
-        self.drawPost()
-        surface.blit(self.surface,(self.x,self.y))
+    def generateNoise(self):
+        for i in range(self.noisesCount):
+            self.noises[i].generateNoise()
+            self.noises[i].noiseArrayValues=self.noises[i].noiseArrayValues*self.impacts[i]
+            self.noiseArrayValues+=self.noises[i].noiseArrayValues
+    def generateSurface(self):
+        for i in range(self.noisesCount):
+            self.noises[i].generateSurface()
+            self.noiseArrayColors+=self.noises[i].noiseArrayColors
+        self.surface = surfarray.make_surface(self.noiseArrayColors)
 
 
-    def drawSelf(self):
-        pass
+    def getSurface(self):
+        return self.surface
 
-    def drawPost(self):
-        for post in self.posts:
-            post.draw(self.surface)
+    def addNoise(self,noise,impact=1):
+        noise.resize(self.WIDTH,self.HEIGHT)
 
-    def updateSelf(self):
-        pass
+        self.noisesCount+=1
+        self.noises.append(noise)
+        self.impacts.append(impact)
+        self.minValue+=noise.minValue*impact
+        self.maxValue+=noise.maxValue*impact
 
-    def updatePost(self):
-        for post in self.posts:
-            post.update()
+        noise.generateNoise()
 
-    def addView(self,view):
-        self.posts.append(view)
+class Filter:
+    def __init__(self,type,filter):
+        self.filter=filter
+        self.type=type
 
-class Button(View):
-    def __init__(self,x,y,width,height):
-        super(Button, self).__init__(x,y,width,height)
-
-        self.onClick=func
-
-    def drawSelf(self):
-        draw.rect(self.surface,(100,100,100),self.surface.get_rect(),3)
 
 class Game:
     def __init__(self):
@@ -119,9 +145,11 @@ class Game:
 
         self.mPosBuf = [0,Vector2(0,0)]
 
-        self.noiseSurface=Surface((700,500))
 
-        self.noise=SimpleNoise(256,256,16)
+        self.noise1=SimpleNoise(256,256,16)
+        self.noise1.generateVectors()
+        self.noise1.generateNoise()
+        self.noise1.generateSurface()
 
     def checkInput(self):
         for even in event.get():
@@ -143,7 +171,7 @@ class Game:
 
     def mainDraw(self):
         self.win.fill(self.BACKGROUND)
-        self.win.blit(self.noise.getSurface(),(0,0))
+        self.win.blit(self.noise1.getSurface(),(0,0))
         draw.rect(self.win,self.BORDER,[0,0,self.WIDTH,self.HEIGHT],self.BORDER_WIDTH)
 
     def mainWindowUpdate(self):
